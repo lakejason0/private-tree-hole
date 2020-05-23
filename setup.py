2cda3d2a7c05d7f50fca87bd22baa58639f7379d
lakejason0 <sunliyuan200402@outlook.com>
2020-05-24 02:46:31 +0800

    Update?

--- setup.py
+++ setup.py
@@ -1,11 +1,37 @@
-from sqlalchemy import Column, String, create_engine
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Boolean, Integer, Time, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import pymysql
 
-Base = declarative_base()
+app = Flask(__name__)
+
+USERNAME = 'treehole'
+PASSWORD = '*******'
+HOST = 'lakejason0.ml'
+PORT = 3306
+DATABASE = 'treehole'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(USERNAME,PASSWORD,HOST,PORT,DATABASE)
+engine = create_engine(DB_URI)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+Base = declarative_base(engine)
 
 class Post(Base):
     __tablename__ = 'post'
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(25))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(25),nullable=False)
+    thread = Column(String(30),nullable=False)
+    time = Column(Time,nullable=False)
+    floor = Column(Integer,nullable=False)
+    is_deleted = Column(Boolean,nullable=False)
+    report = Column(Integer,nullable=True)
+
+Base.metadata.create_all(engine)
+
+DBSession = sessionmaker(bind=engine)
